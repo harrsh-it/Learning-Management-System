5726c9a4efc5b3b4b4ffedc81ea256427c11db79
harrsh_it <harshitdharmveer@gmail.com>
2026-01-30 22:03:24 +0530

    Implemented the Capmanions Library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { getAllComapanion } from '../../lib/actions/companion.actions';
+import CompanionCard from '@/components/CompanionCard';
+import { getSubjectColor } from '../../lib/utils';
+import SearchInput from '@/components/SearchInput';
+import SearchFilter from '@/components/SearchFilter';
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({searchParams} : SearchParams ) => {
+
+    const filters = await searchParams;
+    const subject  = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
+
+    const companions = await getAllComapanion({subject, topic});
+
+    console.log(companions);
+
+
   return (
-    <div>CompanionsLibrary</div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companion Library</h1>
+        <div className='flex gap-4'>
+          <SearchInput/>
+          <SearchFilter/>
+        </div>
+      </section>
+      <section className='companion-grid'>
+        {companions.map((companion)=>(
+          <CompanionCard 
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
   )
 }
 
